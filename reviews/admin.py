093a58ab1220c92cb7a255ea2c1c0dcb566d9e98
Ellis Taylor <ellis.taylor@live.com>
2024-03-14 17:05:24 +0000

    add initial gamereview and genre model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import GameReview, Genre
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+@admin.register(GameReview)
+class GameReviewAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, fields for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
+    list_display = ('title', 'slug', 'approved', 'created_on')
+    search_fields = ['title', 'review']
+    list_filter = ('approved','created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('review',)
+
+
+admin.site.register(Genre)
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class GameReview(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='game_reviews',null=True)
+    title = models.CharField(max_length=100, null=False, blank=False)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, null=True, blank=False)
+    description = models.TextField(null=False, blank=False)
+    featured_image = CloudinaryField("image", default="placeholder")
+    review = models.TextField(null=False, blank=False)
+    rating = models.PositiveIntegerField(default=1, choices=[(i, i) for i in range(1, 6)]) 
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:  # Generate slug only if it's not already set
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
