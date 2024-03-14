f5b7929fe32a5c792b9981e69c7c511fb9fe60c0
Ellis Taylor <ellis.taylor@live.com>
2024-03-14 21:06:53 +0000

    add about page view/url and link to template, add review_detail view/url and link to template

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
     path('all-reviews/', views.AllReviews.as_view(), name='all-reviews'),
+    path('review/<int:gamereview_id>/', views.review_detail, name='review_detail'),
 
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -11,7 +11,29 @@ from .models import GameReview
 def index(request):
     return render(request, 'reviews/index.html')
 
+def about(request):
+    return render(request, 'reviews/about.html')
+
 class AllReviews(generic.ListView):
     queryset = GameReview.objects.filter(approved=True)
     template_name = "reviews/all_reviews.html"
     paginate_by = 6
+
+def review_detail(request, gamereview_id):
+    """
+    The purpose of this view is to display the full details of
+    a particular event. Reviews for the selected event will
+    also be shown at the bottom of the current event details.
+    Providing the currently logged in user isn't the user who
+    created the event, they can also submit a new review which
+    will be put up for approval by an admin.
+    """
+    gamereview = get_object_or_404(GameReview, pk=gamereview_id)
+    
+
+    context = {
+        'gamereview': gamereview,
+        
+    }
+
+    return render(request, 'reviews/review_detail.html', context)
