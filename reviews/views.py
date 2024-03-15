2ad8468d9b84bd942026ae56abf66203988c4220
Ellis Taylor <ellis.taylor@live.com>
2024-03-15 02:09:04 +0000

    alter review_detail view docstring

--- reviews/views.py
+++ reviews/views.py
@@ -22,7 +22,7 @@ class AllReviews(generic.ListView):
 def review_detail(request, gamereview_id):
     """
     The purpose of this view is to display the full details of
-    a particular event. Reviews for the selected event will
+    a particular review. Reviews for the selected event will
     also be shown at the bottom of the current event details.
     Providing the currently logged in user isn't the user who
     created the event, they can also submit a new review which
